0a82662b00e7fd39e3a080fbdc2428f71d3593e1
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2022-10-20 02:05:29 +0300

    fix vimrc.vim name in setup nvim

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -6,7 +6,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "source ~/vim-fiddle/vim/.vimrc" > ~/.vimrc
+echo "source ~/vim-fiddle/vim/vimrc.vim" > ~/.vimrc
 
 echo "Vim config setup successful"
 
