92e9cc4ea27dd8867f3199871b3e63e7918a62ff
HabibAhmedBhutto <habibbhutto@gmail.com>
2022-10-31 02:03:39 +0300

    start using lua for neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+vim.cmd 'source ~/vim-fiddle/nvim/plugins.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/basics.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/colorscheme.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/treesitter.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/telescope.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/fzf.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/keymaps.vim'
+vim.cmd 'source ~/vim-fiddle/nvim/nvim-lsp.vim'
+
