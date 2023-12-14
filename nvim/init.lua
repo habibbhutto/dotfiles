de1cd1faf8b758007aa50ec96c081200b4d52658
Habib Bhutto <habibbhutto@gmail.com>
2023-12-15 01:16:11 +0300

    remove nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ vim.loader.enable()
 vim.cmd([[
    autocmd VimEnter * source ~/vim-fiddle/nvim/colorscheme.vim
    " autocmd VimEnter * lua require('treesitter')
-   autocmd VimEnter * lua require('tree')
+   " autocmd VimEnter * lua require('tree')
 ]]);
 
 vim.cmd 'source ~/vim-fiddle/nvim/plugins.vim'
