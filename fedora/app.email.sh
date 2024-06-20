21e4c012a04f142005721af1c56cef223ad6b443
Habib Bhutto <habibbhutto@gmail.com>
2024-06-20 15:02:52 +0300

    install email applications
    for now let's experiment with thunderbird and geary,
    in future I might choose one depends how it goes.
    
    Thunderbird looks more promising in number of use cases.

---
+++ fedora/app.email.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# Got to check project status and activities
+sudo dnf install -y geary
+
+# Feels like breeze
+# Issues with Google tasks, chats, etc
+sudo dnf install -y thunderbird
+
+# It seems clone of Geary
+# sudo dnf install -y elementary-mail
+
+# Little complicated
+# sudo dnf install -y evolution
