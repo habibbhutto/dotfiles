1a32bf8b565cdddfef871cb761d41296a1ce6243
Habib Bhutto <habibbhutto@gmail.com>
2024-06-20 21:02:07 +0300

    install evolution to evalute all 3 clients
    evolution, thunderbird, and geary

--- fedora/app.email.sh
+++ fedora/app.email.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+# TODO:
+# How to save all email data locally for offline use?
+# -- What's the best approach pop3 or imap?
+# How to save all the clendar information locally?
+# -- Shall I really store calendar information locally?
+
 # Got to check project status and activities
 sudo dnf install -y geary
 
@@ -11,4 +17,4 @@ sudo dnf install -y thunderbird
 # sudo dnf install -y elementary-mail
 
 # Little complicated
-# sudo dnf install -y evolution
+sudo dnf install -y evolution
