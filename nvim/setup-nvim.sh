f4f4ce942b511bd755d5baeafa7148c7eeb57a71
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2024-08-23 21:53:46 +0300

    update setup files for vim and neovim

--- nvim/setup-nvim.sh
+++ nvim/setup-nvim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s $(pwd)/nvim ~/.config/
+ln -s $(pwd) ~/.config/
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Neovim config setup successful"
+echo "Neovim configuration is ready"
 
--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s $(pwd)/vim ~/.vim
+ln -s $(pwd) ~/.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Vim config setup successful"
+echo "Vim configuration is ready"
 
