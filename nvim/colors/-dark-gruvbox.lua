2bc02a178f514bed5b61d625c27d41299f924ca2
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2024-09-13 22:14:03 +0300

    make dark gruvbox darker

--- nvim/colors/-dark-gruvbox.lua
+++ nvim/colors/-dark-gruvbox.lua
@@ -4,7 +4,8 @@ vim.o.background = "dark"
 vim.cmd.source("~/dotfiles/nvim/.plugged/gruvbox-material/colors/gruvbox-material.vim")
 vim.cmd [[hi! floatborder guifg=fg guibg=none]]
 
-vim.g.gruvbox_material_foreground = 'mix'
+vim.g.gruvbox_material_foreground = 'orginal'
+vim.g.gruvbox_material_background = 'hard'
 
 print(vim.system({ 'tmux', 'run-shell', '/home/habib/.config/tmux/themes/nord-tmux/nord.tmux'}):wait().stderr)
 
