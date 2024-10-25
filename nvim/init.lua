e7611c9b6aa562dc2400fd48adfb4147f89a6b41
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2024-10-25 23:08:08 +0300

    color schemes tweaks for kitty and nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -150,11 +150,11 @@ local auto_dark_mode = require('auto-dark-mode')
 auto_dark_mode.setup({
     update_interval = 1000,
     set_dark_mode = function()
-        vim.cmd.colorscheme("zenwritten")
+        vim.cmd.colorscheme("basic")
         vim.o.background = 'dark'
     end,
     set_light_mode = function()
-        vim.cmd.colorscheme("zenbones")
+        vim.cmd.colorscheme("basic")
         vim.o.background = 'light'
     end,
 })
