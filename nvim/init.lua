f8806a92dea1c22f7b356b19e36d0db9763bd855
Habib Ahmed Bhutto <habibbhutto@gmail.com>
2024-10-31 22:19:26 +0300

    do not auto preview markdowns

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,7 +159,7 @@ auto_dark_mode.setup({
     end,
 })
 
-vim.g.mkdp_auto_start = 1
+vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 0
 vim.g.mkdp_command_for_global = 1
 vim.g.mkdp_combine_preview = 1
